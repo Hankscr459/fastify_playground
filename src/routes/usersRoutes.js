4768cc1d6148b0f62526b6657ea10c0a6f30f843
Hankscr459 <defcw58@gmail.com>
2021-04-16 13:14:44 +0800

    Create my first schema

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,13 +1,15 @@
 const { Hello } = require('../controller/user')
 
+const { HelloSchema } = require('../schema/users')
+
 const { 
   midd1,
   midd2
 } = require('../middleware/helper')
 
 const routes  = async (fastify, opts) => {
-
-  fastify.get('/', {preHandler: [midd1, midd2]}, Hello)
+  
+  fastify.get('/', { schema: HelloSchema, preHandler: [midd1, midd2]}, Hello)
     
 }
 
---
+++ src/schema/users.js
@@ -0,0 +1,24 @@
+const HelloSchema = {
+    // body: {
+    //   type: 'object',
+    //   // additionalProperties: false, // it will remove all the field that is NOT in the JSON schema
+    //   required: [
+    //     'message'
+    //   ],
+    //   properties: {
+    //     message: { type: 'string' },
+    //   }
+    // },
+    response: {
+        201: {
+        type: 'object',
+        properties: {
+            message: { type: 'string' }
+        }
+        }
+    }
+}
+
+module.exports = {
+    HelloSchema
+}

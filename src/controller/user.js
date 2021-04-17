dac001fb29d3b9b44487fc433a1be1252ee21703
Hankscr459 <defcw58@gmail.com>
2021-04-17 14:52:20 +0800

    aggregate playground_01

--- src/controller/user.js
+++ src/controller/user.js
@@ -20,16 +20,37 @@ const getAllUser = async (req, res) => {
 }
 
 
+// const filterPoint = async (req, res) => {
+//     // Try to use aggregate
+//     const result = await User.aggregate([
+//         { "$match": { bonusPoint: { "$gte": 500 }} },
+//         { "$project": { password: false } }
+//     ]).exec((err, user) => {
+//         try {
+//             res.code(201).send(user)
+//         } catch (err) {
+//             res.code(404).send(err)
+//         }
+//     })
+// }
+
 const filterPoint = async (req, res) => {
-    // Try to use aggregate
-    const result = await User.aggregate([{ "$match": { bonusPoint: { "$gte": 500 }} }]).exec((err, user) => {
+
+    const result = await User.aggregate([
+        { "$match": { bonusPoint: { "$gte": 500 }} },   // bonuspoint must be at least 500 
+        { "$group": {
+             "_id": "$keyboard",
+             "average": { "$avg": "$bonusPoint" }, // average bonuspoint
+             "sum": { "$sum": "$bonusPoint" }   // sum
+            } 
+        }
+    ]).exec((err, user) => {
         try {
             res.code(201).send(user)
         } catch (err) {
             res.code(404).send(err)
         }
     })
-    
 }
 
 

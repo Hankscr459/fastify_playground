cf5cee388f4b13ecd28d3f3636ebf8080e95c300
Hankscr459 <defcw58@gmail.com>
2021-04-18 16:49:01 +0800

    Create  Products pages

--- src/controller/product.js
+++ src/controller/product.js
@@ -1,16 +1,43 @@
 import Product from '../models/productModel.js'
 
-const getProducts = async (req, res) => {
+// const getProducts = async (req, res) => {
     
-    await Product.find()
-        .populate('category')
-        .exec((err, product) => {
-            try {
-                res.code(201).send(product)
-            } catch (err) {
-                res.code(404).send(err)
-            }
-        })
+//     await Product.find()
+//         .populate('category', 'name _id')
+//         .exec((err, product) => {
+//             try {
+//                 res.code(201).send(product)
+//             } catch (err) {
+//                 res.code(404).send(err)
+//             }
+//         })
+    
+// }
+
+
+const getProducts = async (req, res) => {
+    const pageSize = 4
+    let page = Number(req.query.pageNumber) || 1
+
+    const keyboard = req.query.keyboard
+    ? {
+        name: {
+            $regex: req.query.keyboard,
+            $option: 'i'
+        }
+    } : {}
+
+    const count = await Product.countDocuments({...keyboard})
+    const pages = Math.ceil(count / pageSize)
+
+    page = (page > pages) ? pages : page
+
+    const products = await Product.find({...keyboard})
+        .populate('category', 'name _id')
+        .limit(pageSize)
+        .skip(pageSize * (page -1))
+
+    res.code(201).send({products, page, pages })
     
 }
 

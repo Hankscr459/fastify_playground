289853d1a7d85f025186c93fa16d43fce25e778e
Hankscr459 <defcw58@gmail.com>
2021-04-18 19:44:00 +0800

    Display page number

--- src/controller/product.js
+++ src/controller/product.js
@@ -15,9 +15,37 @@ import Product from '../models/productModel.js'
 // }
 
 
+// const getProducts = async (req, res) => {
+//     const pageSize = 4
+//     let page = Number(req.query.pageNumber) || 1
+
+//     const keyboard = req.query.keyboard
+//     ? {
+//         name: {
+//             $regex: req.query.keyboard,
+//             $options: 'i'
+//         }
+//     } : {}
+
+//     const count = await Product.countDocuments({...keyboard})
+//     const pages = Math.ceil(count / pageSize)
+
+//     page = (page > pages) ? pages : page
+
+//     const products = await Product.find({...keyboard})
+//         .populate('category', 'name _id')
+//         .limit(pageSize)
+//         .skip(pageSize * (page -1))
+
+//     res.code(201).send({products, page, pages })
+    
+// }
+
 const getProducts = async (req, res) => {
-    const pageSize = 4
-    let page = Number(req.query.pageNumber) || 1
+    const pageSize = 10
+    let currentPage = Number(req.query.pageNumber) || 1
+    let next = false
+    let prev = false
 
     const keyboard = req.query.keyboard
     ? {
@@ -28,18 +56,27 @@ const getProducts = async (req, res) => {
     } : {}
 
     const count = await Product.countDocuments({...keyboard})
-    const pages = Math.ceil(count / pageSize)
+    const totalPages = Math.ceil(count / pageSize)
 
-    page = (page > pages) ? pages : page
+    currentPage = (currentPage > totalPages) ? totalPages : currentPage
+    next = (currentPage < totalPages) ? true : false
+    prev = (currentPage > 1) ? true : false
 
     const products = await Product.find({...keyboard})
+        .select('-description')
         .populate('category', 'name _id')
         .limit(pageSize)
-        .skip(pageSize * (page -1))
+        .skip(pageSize * (currentPage -1))
 
-    res.code(201).send({products, page, pages })
+    let pageStart = (currentPage > 3) ? currentPage - 3  : 1
+    let pageEnd = (currentPage < (totalPages - 3)) ? currentPage + 3 : totalPages + 1
+
+    const pages = Array(pageEnd - pageStart).fill().map((_, idx) => pageStart + idx)
+
+    res.code(201).send({products, currentPage, totalPages, next, prev, pages })
     
 }
+
 
 export {
     getProducts

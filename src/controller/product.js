230995d7643011dc9e55c5d8b35c3e4e7046ee74
Hankscr459 <defcw58@gmail.com>
2021-04-18 21:28:06 +0800

    Create FilterProducts API

--- src/controller/product.js
+++ src/controller/product.js
@@ -42,10 +42,12 @@ import Product from '../models/productModel.js'
 // }
 
 const getProducts = async (req, res) => {
-    const pageSize = 10
+
+    const sort = req.query.sortBy ? req.query.sortBy : "priceOrderByasc"
+    const parts = sort.split('OrderBy')
+
+    const pageSize = 5
     let currentPage = Number(req.query.pageNumber) || 1
-    let next = false
-    let prev = false
 
     const keyboard = req.query.keyboard
     ? {
@@ -59,19 +61,76 @@ const getProducts = async (req, res) => {
     const totalPages = Math.ceil(count / pageSize)
 
     currentPage = (currentPage > totalPages) ? totalPages : currentPage
-    next = (currentPage < totalPages) ? true : false
-    prev = (currentPage > 1) ? true : false
 
-    const products = await Product.find({...keyboard})
-        .select('-description')
-        .populate('category', 'name _id')
-        .limit(pageSize)
-        .skip(pageSize * (currentPage -1))
-
+    let next = (currentPage < totalPages) ? true : false
+    let prev = (currentPage > 1) ? true : false
     let pageStart = (currentPage > 3) ? currentPage - 3  : 1
     let pageEnd = (currentPage < (totalPages - 3)) ? currentPage + 3 : totalPages + 1
 
     const pages = Array(pageEnd - pageStart).fill().map((_, idx) => pageStart + idx)
+
+    const products = await Product.find({...keyboard})
+        .select('-description')
+        .populate('category', 'name _id')
+        .sort([[parts[0], parts[1]]])
+        .limit(pageSize)
+        .skip(pageSize * (currentPage -1))
+
+    res.code(201).send({products, currentPage, totalPages, next, prev, pages })
+    
+}
+
+
+const getFilterProducts = async (req, res) => {
+
+    const sort = req.query.sortBy ? req.query.sortBy : "priceOrderByasc"
+    const parts = sort.split('OrderBy')
+    let findArgs = {}
+
+    const pageSize = 5
+    let currentPage = Number(req.query.pageNumber) || 1
+
+    const keyboard = req.query.keyboard
+    ? {
+        name: {
+            $regex: req.query.keyboard,
+            $options: 'i'
+        }
+    } : {}
+
+    for (let key in req.body.filters) {
+        if (req.body.filters[key].length > 0) {
+            if (key === "price") {
+                // gte -  greater than price [0-10]
+                // lte - less than
+                findArgs[key] = {
+                    $gte: req.body.filters[key][0],
+                    $lte: req.body.filters[key][1]
+                };
+            } else {
+                findArgs[key] = req.body.filters[key];
+            }
+        }
+    }
+
+    const count = await Product.countDocuments({...keyboard, ...findArgs})
+    const totalPages = Math.ceil(count / pageSize)
+
+    currentPage = (currentPage > totalPages) ? totalPages : currentPage
+
+    let next = (currentPage < totalPages) ? true : false
+    let prev = (currentPage > 1) ? true : false
+    let pageStart = (currentPage > 3) ? currentPage - 3  : 1
+    let pageEnd = (currentPage < (totalPages - 3)) ? currentPage + 3 : totalPages + 1
+
+    const pages = Array(pageEnd - pageStart).fill().map((_, idx) => pageStart + idx)
+
+    const products = await Product.find({...keyboard, ...findArgs})
+        .select('-description')
+        .populate('category', 'name _id')
+        .sort([[parts[0], parts[1]]])
+        .limit(pageSize)
+        .skip(pageSize * (currentPage -1))
 
     res.code(201).send({products, currentPage, totalPages, next, prev, pages })
     
@@ -79,5 +138,6 @@ const getProducts = async (req, res) => {
 
 
 export {
-    getProducts
+    getProducts,
+    getFilterProducts
 }
--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,7 +1,11 @@
-import { getProducts } from '../controller/product.js'
+import { 
+  getProducts,
+  getFilterProducts
+} from '../controller/product.js'
 
 const routes = async (fastify, opts) => {
-    fastify.get('/getProducts', getProducts)
-  }
+  fastify.get('/getProducts', getProducts)
+  fastify.post('/getProducts', getFilterProducts)
+}
   
   export default  routes 

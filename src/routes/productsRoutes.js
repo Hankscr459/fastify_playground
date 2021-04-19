874a4cad5bd6deb8c72f9eeec005b000e0de9a9e
Hankscr459 <defcw58@gmail.com>
2021-04-19 13:36:43 +0800

    Create querystringSchema

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -4,12 +4,13 @@ import {
 } from '../controller/product.js'
 
 import {
-  getProductsSchema
+  getProductsSchema,
+  getFilterProductsSchema
 } from '../schema/products.js'
 
 const routes = async (fastify, opts) => {
   fastify.get('/getProducts', { schema: getProductsSchema }, getProducts)
-  fastify.post('/getFilterProducts', getFilterProducts)
+  fastify.post('/getFilterProducts', { schema: getFilterProductsSchema }, getFilterProducts)
 }
   
   export default  routes 
--- src/schema/products.js
+++ src/schema/products.js
@@ -1,43 +1,83 @@
-const getProductsSchema = {
-    response: {
-        201: {
-        type: 'object',
-        properties: {
-                products: {
-                    type: 'array',
-                    items: {
-                        type: 'object',
-                        properties: {
-                            rating: { type: 'number' },
-                            numReviews: { type: 'number' },
-                            price: { type: 'number' },
-                            countInStock: { type: 'number' },
+const productsResponse = {
+
+    201: {
+    type: 'object',
+    properties: {
+            products: {
+                type: 'array',
+                items: {
+                    type: 'object',
+                    properties: {
+                        rating: { type: 'number' },
+                        numReviews: { type: 'number' },
+                        price: { type: 'number' },
+                        countInStock: { type: 'number' },
+                        _id: { type: 'string' },
+                        name: { type: 'string' },
+                        image: { type: 'string' },
+                        brand: { type: 'string' },
+                        reviews: { type: ['object', 'null', 'array'] },
+                        user: { type: 'string' },
+                        category: {
                             _id: { type: 'string' },
-                            name: { type: 'string' },
-                            image: { type: 'string' },
-                            brand: { type: 'string' },
-                            reviews: { type: ['object', 'null', 'array'] },
-                            user: { type: 'string' },
-                            category: {
-                                _id: { type: 'string' },
-                                name: { type: 'string' }
-                            },
-                            createdAt: { type: 'string' },
-                            updatedAt: { type: 'string' }
-                        }
+                            name: { type: 'string' }
+                        },
+                        createdAt: { type: 'string' },
+                        updatedAt: { type: 'string' }
                     }
-                    
-                },
-                currentPage: { type: 'number' },
-                totalPages: { type: 'number' },
-                next: { type: 'boolean' },
-                prev: { type: 'boolean' },
-                pages: { type: 'array' }
-            }
+                }
+                
+            },
+            currentPage: { type: 'number' },
+            totalPages: { type: 'number' },
+            next: { type: 'boolean' },
+            prev: { type: 'boolean' },
+            pages: { type: 'array' }
         }
     }
+    
+}
+
+const productsQuerystring = {
+
+    type: 'object',
+    properties: {
+        pageNumber: { type: 'number' },
+        sortBy: { type: 'string' }
+    }
+
+}
+
+const getProductsSchema = {
+
+    querystring: productsQuerystring,
+    response: productsResponse
+
+}
+
+const getFilterProductsSchema = {
+
+    body: {
+      // additionalProperties: false, // it will remove all the field that is NOT in the JSON schema
+    //   required: [
+    //   ],
+      type: 'object',
+      properties: {
+        filters: {
+            type: 'object',
+            properties: {
+                category: { type: 'array' },
+                price: { type: 'array' }
+            }
+        }
+      }
+    },
+    querystring: productsQuerystring,
+    response: productsResponse
+
 }
 
 export {
-    getProductsSchema
+    getProductsSchema,
+    getFilterProductsSchema
 }
